c2ddece05ac31e5c73a3506fa3f2a8619a2d011d
marcosvillabasa <marcosvillabasa@gmail.com>
2018-10-10 14:34:59 -0300

    Primera etapa lista Componentes compartidos listos: header y footer Componente Home listo Componente About listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/shared/header/header.component';
+import { FooterComponent } from './componentes/shared/footer/footer.component';
+import { AboutComponent } from './componentes/pages/about/about.component';
+import { HomeComponent } from './componentes/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
